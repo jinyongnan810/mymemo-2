9f78fee2cea12c4b4c148602b9f766a2ddd8172a
Yuunan Kin <yuunan.kin@gmail.com>
2020-09-23 08:53:46 +0900

    add cors

--- config/middleware.js
+++ config/middleware.js
@@ -3,9 +3,12 @@ module.exports = {
   settings: {
     cors: {
       origin: [
-        'http://localhost', 'https://y-kin.com', 'https://www.y-kin.com',
-        'https://jinyongnan810.github.io',
-        'https://friendly-sinoussi-bb023c.netlify.app'
+        "http://localhost",
+        "http://localhost:3000",
+        "https://y-kin.com",
+        "https://www.y-kin.com",
+        "https://jinyongnan810.github.io",
+        "https://friendly-sinoussi-bb023c.netlify.app",
       ],
     },
   },
