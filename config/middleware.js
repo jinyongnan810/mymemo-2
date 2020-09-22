fbe8031a7d8a119e8b2aca3c6dc0a2c74407cf9b
Yuunan Kin <yuunan.kin@gmail.com>
2020-09-22 20:41:19 +0900

    mymemo -2

---
+++ config/middleware.js
@@ -0,0 +1,12 @@
+module.exports = {
+  //...
+  settings: {
+    cors: {
+      origin: [
+        "http://localhost",
+        "https://y-kin.com",
+        "https://jinyongnan810.github.io",
+      ],
+    },
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c522ad85-5d8d-4307-9ed9-bfd7c2b6385c'
+};
