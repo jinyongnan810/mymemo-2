c21be7e37ef97118000d58c60a88cfadac7acde7
Yuunan Kin <yuunan.kin@gmail.com>
2020-09-23 08:55:56 +0900

    change pwd

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.xk5om.gcp.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'mymemo-2'),
-        username: env('DATABASE_USERNAME', 'jinyongnan'),
-        password: env('DATABASE_PASSWORD', 'jinyongnan'),
+        host: env("DATABASE_HOST", "cluster0.xk5om.gcp.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "mymemo-2"),
+        username: env("DATABASE_USERNAME", "jinyongnan"),
+        password: env("DATABASE_PASSWORD", "QSgeiKvxBISzkQxW"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", "admin"),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
