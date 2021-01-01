ff7678da0da29bdf8c8db90dae0157596467788c
Yuunan Kin <yuunan.kin@gmail.com>
2021-01-01 20:25:01 +0900

    try server upload

--- api/memo/controllers/upload.js
+++ api/memo/controllers/upload.js
@@ -7,16 +7,27 @@ require("dotenv").config();
  */
 
 module.exports = {
-  async signature(ctx) {
-    const timestamp = Math.round(new Date().getTime() / 1000);
-    var signature = cloudinary.utils.api_sign_request(
-      {
-        timestamp: timestamp,
-        upload_preset: "ml_default",
-      },
-      process.env.API_SECRET
-    );
+  async upload(ctx) {
+    // sign and try cloud upload. didn't work
+    // const timestamp = Math.round(new Date().getTime() / 1000);
+    // var signature = cloudinary.utils.api_sign_request(
+    //   {
+    //     timestamp: timestamp,
+    //     upload_preset: "ml_default",
+    //   },
+    //   process.env.API_SECRET
+    // );
 
-    ctx.send({ timestamp, signature });
+    // ctx.send({ timestamp, signature });
+    const req = ctx.request.body;
+    const file = req.data;
+    try {
+      const uploadResponse = await cloudinary.uploader.upload(file, {
+        upload_preset: "ml_default",
+      });
+      ctx.send(uploadResponse);
+    } catch (error) {
+      console.log("Error uploading:", error);
+    }
   },
 };
